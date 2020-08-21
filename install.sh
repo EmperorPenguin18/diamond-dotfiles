cb82c876e5ee71dfb3643ac709d56b51888a0b8d
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-08-20 20:39:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,33 +42,34 @@ pacstrap /mnt base sudo vim grub parted pacman-contrib btrfs-progs amd-ucode int
 genfstab -U /mnt >> /mnt/etc/fstab
 
 #Chroot into system
-arch-chroot /mnt
+arch-chroot /mnt << "EOT"
 
-#Set localization stuff
-ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
-hwclock --systohc
-locale-gen
-echo "LANG=en_CA.UTF-8" > /etc/locale.conf
-
-#Network stuff
-echo "Sebs-PC" > /etc/hostname
-echo "127.0.0.1   localhost" > /etc/hosts
-echo "::1   localhost" >> /etc/hosts
-echo "127.0.1.1   Sebs-PC.localdomain  Sebs-PC" >> /etc/hosts
-systemctl enable dhcpcd
-
-#Create root password
-passwd
-
-#Create user
-useradd -m sebastien
-passwd sebastien
-usermod -aG wheel,audio,video,optical,storage sebastien
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-#Create bootloader
-grub-install /dev/$DISKNAME
-grub-mkconfig -o /boot/grub/grub.cfg
-
+   #Set localization stuff
+   ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
+   hwclock --systohc
+   locale-gen
+   echo "LANG=en_CA.UTF-8" > /etc/locale.conf
+   
+   #Network stuff
+   echo "Sebs-PC" > /etc/hostname
+   echo "127.0.0.1   localhost" > /etc/hosts
+   echo "::1   localhost" >> /etc/hosts
+   echo "127.0.1.1   Sebs-PC.localdomain  Sebs-PC" >> /etc/hosts
+   systemctl enable dhcpcd
+   
+   #Create root password
+   passwd
+   
+   #Create user
+   useradd -m sebastien
+   passwd sebastien
+   usermod -aG wheel,audio,video,optical,storage sebastien
+   echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+   
+   #Create bootloader
+   grub-install /dev/$DISKNAME
+   grub-mkconfig -o /boot/grub/grub.cfg
+   
 #Done
+EOT
 reboot
