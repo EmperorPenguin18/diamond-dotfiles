d5e8cbf2990354daa267a9b34d9363a806adc74a
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-08-24 17:24:06 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#Setup pacman
+
+
+#Setup X Server
+
+
+#Setup awesomewm
+
+
+#Setup rclone mounts
+
+
+#Setup updates + backups
+
+
+#Setup Plymouth
+
+
+#Setup terminal emulator
+
+
+#Setup file manager
+
+
+#Setup web browser
+
+
+#Setup nvidia drivers
+
+
+#Setup gaming
