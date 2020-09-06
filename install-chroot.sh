0397a41851de38738c025983aae1efd5767b3641
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-05 20:36:06 -0400

    Update install-chroot.sh

--- install-chroot.sh
+++ install-chroot.sh
@@ -1,3 +1,8 @@
+#Get the diskname
+ls /dev
+echo "Enter the name of the install disk (e.g. /dev/sda)"
+read DISKNAME
+
 #Set localization stuff
 ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 hwclock --systohc
@@ -22,5 +27,7 @@ usermod -aG wheel,audio,video,optical,storage sebastien
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 #Create bootloader
+mkdir /boot/EFI
+mount /dev/$(echo $DISKNAME)1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
