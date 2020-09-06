d77870fc42e751f6627d34a0d5e2ea6bee671d6e
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-05 20:02:28 -0400

    Update install-chroot.sh

--- install-chroot.sh
+++ install-chroot.sh
@@ -1,6 +1,7 @@
 #Set localization stuff
 ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 hwclock --systohc
+echo "en_CA.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_CA.UTF-8" > /etc/locale.conf
 
@@ -9,7 +10,7 @@ echo "Sebs-PC" > /etc/hostname
 echo "127.0.0.1   localhost" > /etc/hosts
 echo "::1   localhost" >> /etc/hosts
 echo "127.0.1.1   Sebs-PC.localdomain  Sebs-PC" >> /etc/hosts
-systemctl enable dhcpcd
+systemctl enable NetworkManager
 
 #Create root password
 passwd
@@ -21,5 +22,5 @@ usermod -aG wheel,audio,video,optical,storage sebastien
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 #Create bootloader
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
