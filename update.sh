439364551c98abef00b6bd52bc9c512f4857c881
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-06 16:13:57 -0400

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#Backup system
+btrfs subvolume snapshot / /home/sebastien/.snapshots/"$(echo date)"
+rclone sync --config=/home/sebastien/.config/rclone/rclone.conf /home/sebastien/.snapshots onedriveschool:/Backups/
+
+#Update system
+curl 'https://www.archlinux.org/mirrorlist/?country=CA&protocol=http&protocol=https&ip_version=4' > /etc/pacman.d/mirrorlist
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+rankmirrors /etc/pacman.d/mirrorlist >> /etc/pacman.d/mirrorlist
+yay -Syyuq --combinedupgrade --noconfirm > output.txt
+curl -X POST https://textbelt.com/text \
+   --data-urlencode phone='6137200482' \
+   --data-urlencode message=$(cat output.txt) \
+   -d key=textbelt
+rm output.txt
+
+#Finish
+reboot
