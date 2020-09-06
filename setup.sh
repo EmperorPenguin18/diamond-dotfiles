13c1c3a92c7af0ea129964df8c85ce2c8c08c0dc
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-05 22:43:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,32 @@
+#Check if script has root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 #Setup pacman
-
-
-#Setup Plymouth
-
-
-#Setup login manager
-
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+echo "" >> /etc/pacman.conf
+echo "Color" >> /etc/pacman.conf
+echo "ILoveCandy" >> /etc/pacman.conf
+pacman -Sy --needed base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ../
+rm -r yay
+#*Optimize compiling*
 
 #Setup rclone mounts
-
+pacman -S rclone
+mv rclone.conf /home/sebastien/.config/rclone/rclone.conf
+mv rclone1.service /etc/systemd/system/rclone1.service
+mv rclone2.service /etc/systemd/system/rclone2.service
+mv rclone3.service /etc/systemd/system/rclone3.service
+systemctl enable rclone1
+systemctl enable rclone2
+systemctl enable rclone3
 
 #Setup updates + backups
 
@@ -16,19 +34,50 @@
 #Setup X Server
 
 
+#Setup login manager
+
+
+#Setup Plymouth
+yay -S plymouth
+echo "MODULES()" > /etc/mkinitcpio.conf
+echo "BINARIES()" >> /etc/mkinitcpio.conf
+echo "FILES()" >> /etc/mkinitcpio.conf
+echo "HOOKS=(base udev plymouth autodetect modconf block filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
+sed '6d' /etc/default/grub
+sed '5 a GRUB_CMDLINE_LINUX_DEFAULT="quiet splash loglevel=3 rd.udev.log_priority=3 vt.global_cursor_default=0"' /etc/default/grub
+#*Silent boot*
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl disable lightdm
+systemctl enable lightdm-plymouth
+git clone https://github.com/garak92/powered-plymouth-theme/
+cd powered-plymouth-theme/
+mv powered-plymouth-theme/ /usr/share/plymouth/themes/
+cd ../
+rm -r powered-plymouth-theme
+sed "s/Theme=.*/Theme=powered-plymouth-theme/g" /etc/plymouth/plymouthd.conf
+mkinitcpio -P
+
 #Setup awesomewm
 
 
 #Setup terminal emulator
-
+pacman -S alacritty
+mv alacritty.yml /home/sebastien/.config/alacritty/alacritty.yml
+#*Shell*
+#*Vim*
 
 #Setup file manager
 
 
 #Setup web browser
-
+pacman -S firefox
+#*Read arch wiki page*
 
 #Setup nvidia drivers
 
 
 #Setup gaming
+
+
+#Other
+pacman -S openssh
