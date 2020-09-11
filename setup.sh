614a3d19df8dc08c69178b793ba03971b5946da4
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-10 21:42:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ yay -S plymouth
 echo "MODULES()" > /etc/mkinitcpio.conf
 echo "BINARIES()" >> /etc/mkinitcpio.conf
 echo "FILES()" >> /etc/mkinitcpio.conf
-echo "HOOKS=(base udev plymouth autodetect modconf block filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
+echo "HOOKS=(base udev plymouth autodetect modconf block btrfs filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
 sed '6d' /etc/default/grub
 sed '5 a GRUB_CMDLINE_LINUX_DEFAULT="quiet splash loglevel=3 rd.udev.log_priority=3 vt.global_cursor_default=0"' /etc/default/grub
 #*Silent boot*
