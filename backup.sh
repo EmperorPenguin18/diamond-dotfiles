9db2c56edf127143e915cf0f95b95a7f220037fd
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-10 22:10:46 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#Backup system
+DISKNAME=$(lsblk | grep disk | awk '{print $1;}')
+mkdir /mnt/_active
+mount -o subvol=_active/rootvol /dev/$(echo $DISKNAME)2 /mnt/_active/
+btrfs subvolume snapshot -r /mnt/_active /home/sebastien/.snapshots/"$(date "+%F")"
+rclone sync --config=/home/sebastien/.config/rclone/rclone.conf /home/sebastien/.snapshots onedriveschool:/Backups/
+umount /mnt/_active
+rmdir /mnt/_active
