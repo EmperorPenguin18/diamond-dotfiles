0455355b8188918dd68f2d25e3524707ab346289
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-10 22:20:52 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #Update system
-curl 'https://www.archlinux.org/mirrorlist/?country=CA&protocol=http&protocol=https&ip_version=4' > /etc/pacman.d/mirrorlist
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
-rankmirrors /etc/pacman.d/mirrorlist >> /etc/pacman.d/mirrorlist
 yay -Syyuuq --combinedupgrade --noconfirm > output.txt
 curl -X POST https://textbelt.com/text \
    --data-urlencode phone='6137200482' \
