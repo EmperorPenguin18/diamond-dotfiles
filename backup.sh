05ed65da8a9bdefb4f160f10763a3fae76264309
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-23 17:12:40 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ btrfs subvolume snapshot -r /mnt/_active /home/sebastien/.snapshots/"$(date "+%F
 rclone sync --config=/home/sebastien/.config/rclone/rclone.conf /home/sebastien/.snapshots onedriveschool:/Backups/
 umount /mnt/_active
 rmdir /mnt/_active
+grub-mkconfig -o /boot/grub/grub.cfg
 
 #Update mirrors
 reflector --country Canada --protocol https --sort rate --save /etc/pacman.d/mirrorlist
