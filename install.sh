d67efdc9d641741723364e50fa3c893e2f54eb6e
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-23 14:52:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 timedatectl set-ntp true
 
 #Partition disk
-pacman -S dmidecode
+pacman -S dmidecode --noconfirm
 DISKNAME=$(lsblk | grep disk | awk '{print $1;}')
 DISKSIZE=$(lsblk --output SIZE -n -d /dev/$DISKNAME | sed 's/.$//')
 MEMSIZE=$(dmidecode -t 17 | grep "Size.*MB" | awk '{s+=$2} END {print s / 1024}')
@@ -45,7 +45,7 @@ mount /dev/$(echo $DISKNAME)1 /mnt/boot/EFI
 mount -o subvol=_active/homevol /dev/$(echo $DISKNAME)2 /mnt/home
 
 #Configure mirrors
-pacman -S reflector
+pacman -S reflector --noconfirm
 reflector --country Canada --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 #Install packages
@@ -53,13 +53,15 @@ pacman -Sy
 pacstrap /mnt base linux linux-firmware sudo vim grub grub-btrfs efibootmgr dosfstools os-prober mtools parted reflector btrfs-progs amd-ucode intel-ucode dmidecode networkmanager git
 
 #Generate FSTAB
-#*Get UUIDs*
-echo "UUID= /  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=/_active/rootvol   0  0" >> /mnt/etc/fstab
-echo "UUID= /boot/EFI   vfat  rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro   0  2" >> /mnt/etc/fstab
-echo "UUID= /tmp  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_active/tmp  0  0" >> /mnt/etc/fstab
-echo "UUID= /home btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_active/homevol   0  0" >> /mnt/etc/fstab
-echo "UUID= none  swap  defaults 0  0" >> /mnt/etc/fstab
-echo "UUID= /home/sebastien/.snapshots btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_snapshots 0  0" >> /mnt/etc/fstab
+UUID1=$(blkid -s UUID -o value /dev/$(echo $DISKNAME)1)
+UUID2=$(blkid -s UUID -o value /dev/$(echo $DISKNAME)2)
+UUID3=$(blkid -s UUID -o value /dev/$(echo $DISKNAME)3)
+echo UUID=$UUID2 /  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=/_active/rootvol   0  0 >> /mnt/etc/fstab
+echo UUID=$UUID1 /boot/EFI   vfat  rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro   0  2 >> /mnt/etc/fstab
+echo UUID=$UUID2 /tmp  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_active/tmp  0  0 >> /mnt/etc/fstab
+echo UUID=$UUID2 /home btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_active/homevol   0  0 >> /mnt/etc/fstab
+echo UUID=$UUID3 none  swap  defaults 0  0 >> /mnt/etc/fstab
+echo UUID=$UUID2 /home/sebastien/.snapshots btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache,subvol=_snapshots 0  0 >> /mnt/etc/fstab
 
 #Chroot into system
 chmod +x install-chroot.sh
