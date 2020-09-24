7ba656e3ac8dcef2fd0d113f2b47665906a51176
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-09-23 22:58:05 -0400

    Update install-chroot.sh

--- install-chroot.sh
+++ install-chroot.sh
@@ -1,3 +1,9 @@
+#Check if script has root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 #Add btrfs to HOOKS
 echo "MODULES=()" > /etc/mkinitcpio.conf
 echo "BINARIES=()" >> /etc/mkinitcpio.conf
