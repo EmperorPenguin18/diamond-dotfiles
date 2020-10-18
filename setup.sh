afa19cd146c8a202ccafe0f489543633c7dea94b
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-10-18 13:39:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -106,12 +106,12 @@ fc-cache
 #mv left.rasi /usr/share/rofi/themes/
 #mv center.rasi /usr/share/rofi/themes/
 mkdir /home/sebastien/.config/rofi
-#mv config.rasi /home/sebastien/.config/rofi/
-#mv rofi-top.sh /home/sebastien/
-#mv rofi-right.sh /home/sebastien/
-#mv rofi-bottom.sh /home/sebastien/
-#mv rofi-left.sh /home/sebastien/
-#mv rofi-center.sh /home/sebastien/
+mv config.rasi /home/sebastien/.config/rofi/
+mv rofi-top.sh /home/sebastien/
+mv rofi-right.sh /home/sebastien/
+mv rofi-bottom.sh /home/sebastien/
+mv rofi-left.sh /home/sebastien/
+mv rofi-center.sh /home/sebastien/
 #*Help/shortcuts*
 
 #Setup terminal emulator
