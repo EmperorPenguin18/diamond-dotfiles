1f18ecb28b19f24690df0c33fe5538b119f15f1d
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-10-24 22:51:42 -0400

    Update rofi-shortcuts.sh

--- windowmanager/rofi-shortcuts.sh
+++ windowmanager/rofi-shortcuts.sh
@@ -1,4 +1,4 @@
 #Shortcuts
-output=$(rofi -dmenu -p Shortcuts -i -input /home/sebastien/kb.txt)
+output=$(rofi -theme center -dmenu -p Shortcuts -i -input /home/sebastien/kb.txt)
 if [[ $output == "" ]]; then exit 1;
 else xdotool key $output; fi
