fbc9156747bf1a13434b297bad49efaecdcb3730
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-10-29 14:27:50 -0400

    Update rofi-left.sh

--- windowmanager/rofi-left.sh
+++ windowmanager/rofi-left.sh
@@ -1,6 +1,6 @@
 #File manager
 cd $@
 output=$(ls -a | rofi -theme left -dmenu -p Files -i)
-if [[ $output == ""]]; then exit 1; fi
+if [[ $output == "" ]]; then exit 1; fi
 if [ -d $output ]; then /home/sebastien/rofi.sh $output; fi
 if [ -f $output ]; then xdg-open $output; fi
