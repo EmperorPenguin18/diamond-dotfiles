951420a67a19d650c36e9af31e3a0e7500ffc02b
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-25 00:37:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,208 +1,253 @@
-#!/bin/bash
+#!/bin/sh
 
-#Check if script has root privileges
+#Check if script is being run as root user
 if [ "$(whoami)" != "root" ]; then
-        echo "Script must be run as user: root"
-        exit 255
+    echo "Script must be run as user: root"
+    exit 255
 fi
 
-#Setup pacman
-mv pacman/pacman.conf /etc/pacman.conf
-echo "permit nopass /usr/bin/pacman" >> /etc/doas.conf
-echo "permit nopass /usr/bin/yay" >> /etc/doas.conf
-echo "permit nopass /usr/bin/makepkg" >> /etc/doas.conf
-sed -i '/MAKEFLAGS/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
-cd ../
-pacman -Sy --needed base-devel --noconfirm
-su sebastien -c "git clone https://aur.archlinux.org/yay.git"
-cd yay
-su sebastien -c "makepkg -si --noconfirm"
-cd ../
-rm -r yay
-cd ArchConfigs
+packagemanager ()
+{
+    cd /home/sebastien
+    mv pacman/pacman.conf /etc/pacman.conf
+    echo "permit nopass /usr/bin/pacman" >> /etc/doas.conf
+    echo "permit nopass /usr/bin/pikaur" >> /etc/doas.conf
+    echo "permit nopass /usr/bin/makepkg" >> /etc/doas.conf
+    sed -i '/MAKEFLAGS/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
+    cd ../
+    pacman -Sy autoconf automake bison flex groff m4 pkgconf pyalpm python-commonmark --noconfirm
+    su sebastien -c "git clone https://aur.archlinux.org/pikaur.git"
+    cd pikaur
+    su sebastien -c "makepkg --noconfirm"
+    pacman -U *.pkg* --noconfirm
+    cd ../
+    rm -r pikaur
+    cd ArchConfigs
+}
 
-#Setup rclone mounts
-pacman -S fuse rclone --noconfirm
-echo "user_allow_other" >> /etc/fuse.conf
-mkdir -p /home/sebastien/.config/rclone
-mv rclone/rclone.conf /home/sebastien/.config/rclone/rclone.conf
-chown sebastien:sebastien /home/sebastien/.config/rclone/rclone.conf
-mv rclone/rclone1.service /etc/systemd/system/rclone1.service
-mv rclone/rclone2.service /etc/systemd/system/rclone2.service
-mv rclone/rclone3.service /etc/systemd/system/rclone3.service
-mkdir /mnt/Personal
-mkdir /mnt/School
-mkdir /mnt/Media
-systemctl enable rclone1
-systemctl enable rclone2
-systemctl enable rclone3
+cloud ()
+{
+    pacman -S fuse rclone --noconfirm
+    echo "user_allow_other" >> /etc/fuse.conf
+    mkdir -p /home/sebastien/.config/rclone
+    mv rclone/rclone.conf /home/sebastien/.config/rclone/rclone.conf
+    chown sebastien:sebastien /home/sebastien/.config/rclone/rclone.conf
+    mv rclone/rclone1.service /etc/systemd/system/rclone1.service
+    mv rclone/rclone2.service /etc/systemd/system/rclone2.service
+    mv rclone/rclone3.service /etc/systemd/system/rclone3.service
+    mkdir /mnt/Personal
+    mkdir /mnt/School
+    mkdir /mnt/Media
+    systemctl enable rclone1
+    systemctl enable rclone2
+    systemctl enable rclone3
+}
 
-#Setup updates + backups
-pacman -S cron reflector --noconfirm
-chmod +x update.sh
-mv update/update.sh /home/sebastien/update.sh
-chmod +x backup.sh
-mv update/backup.sh /home/sebastien/backup.sh
-echo "0 3 * * 1 root /home/sebastien/backup.sh" >> /etc/crontab
-echo "0 4 * * 1 sebastien /home/sebastien/update.sh" >> /etc/crontab
-#*Other system maintenance?*
+update ()
+{
+    pacman -S cron reflector --noconfirm
+    chmod +x update/update.sh
+    mv update/update.sh /home/sebastien/update.sh
+    chmod +x update/backup.sh
+    mv update/backup.sh /home/sebastien/backup.sh
+    echo "0 3 * * 1 root /home/sebastien/backup.sh" >> /etc/crontab
+    echo "0 4 * * 1 sebastien /home/sebastien/update.sh" >> /etc/crontab
+    #*Other system maintenance?*
+}
 
-#Setup X Server
-pacman -S xorg xorg-drivers lib32-mesa lib32-vulkan-icd-loader vulkan-intel lib32-vulkan-intel intel-media-driver libva-intel-driver libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk --noconfirm
-#*Enable vsync, freesync/gsync, hardware acceleration, vulkan etc
-mv xorg/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
-#*Multi-monitor*
+xorg ()
+{
+    pacman -S xorg xorg-drivers lib32-mesa lib32-vulkan-icd-loader vulkan-intel lib32-vulkan-intel intel-media-driver libva-intel-driver libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk --noconfirm
+    #*Enable vsync, freesync/gsync, hardware acceleration, vulkan etc
+    mv xorg/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
+    #*Multi-monitor*
+}
 
-#Setup nvidia drivers
-pacman -S nvidia-prime --noconfirm
-#*prime-run*
+nvidia ()
+{
+    pacman -S nvidia-prime --noconfirm
+    #*prime-run*
+}
 
-#Setup login manager
-yay -S lightdm lightdm-webkit2-greeter lightdm-webkit2-theme-glorious --noconfirm
-mv login/lightdm.conf /etc/lightdm/lightdm.conf
-mv login/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
-mv login/index.html /usr/share/lightdm-webkit/themes/glorious/
-rm /usr/share/backgrounds/*
-mv login/background.png /usr/share/backgrounds/
-mv login/steam-big-picture.desktop /usr/share/xsessions/
-mv login/jellyfin.desktop /usr/share/xsessions/
-#*Theme*
-#*Sessions*
-#*On-screen keyboard*
-#*http://www.mattfischer.com/blog/archives/5*
+login ()
+{
+    pikaur -S lightdm lightdm-webkit2-greeter lightdm-webkit2-theme-glorious --noconfirm
+    mv login/lightdm.conf /etc/lightdm/lightdm.conf
+    mv login/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
+    mv login/index.html /usr/share/lightdm-webkit/themes/glorious/
+    rm -r /usr/share/backgrounds/*
+    mv login/background.png /usr/share/backgrounds/
+    mkdir -p /usr/share/xsessions
+    mv login/steam-big-picture.desktop /usr/share/xsessions/
+    mv login/jellyfin.desktop /usr/share/xsessions/
+    #*Theme*
+    #*Sessions*
+    #*On-screen keyboard*
+    #*http://www.mattfischer.com/blog/archives/5*
+}
 
-#Setup Plymouth
-su sebastien -c "yay -S plymouth --noconfirm"
-echo "MODULES=()" > /etc/mkinitcpio.conf
-echo "BINARIES=()" >> /etc/mkinitcpio.conf
-echo "FILES=(/crypto_keyfile.bin)" >> /etc/mkinitcpio.conf
-echo "HOOKS=(base udev plymouth plymouth-encrypt autodetect modconf block btrfs filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
-mv plymouth/grub /etc/default/grub
-sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/$(lsblk -l | grep part | grep / | awk '{print $1}' | sed -n '2p')):cryptroot\"/g" /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl disable lightdm
-systemctl enable lightdm-plymouth
-git clone https://github.com/garak92/powered-plymouth-theme/
-cd powered-plymouth-theme/
-mv powered-plymouth-theme/ /usr/share/plymouth/themes/
-cd ../
-rm -r powered-plymouth-theme
-sed -i "s/Theme=.*/Theme=powered-plymouth-theme/g" /etc/plymouth/plymouthd.conf
-mkinitcpio -P
+plymouth ()
+{
+    pikaur -S plymouth --noconfirm
+    sed -i '4d' /etc/mkinitcpio.conf
+    echo "HOOKS=(base udev plymouth plymouth-encrypt autodetect modconf block btrfs filesystems keyboard fsck)" >> /etc/mkinitcpio.conf
+    mv plymouth/grub /etc/default/grub
+    sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -o device | xargs -L1 cryptsetup luksUUID):cryptroot\"/g" /etc/default/grub
+    grub-mkconfig -o /boot/grub/grub.cfg
+    systemctl disable lightdm
+    systemctl enable lightdm-plymouth
+    git clone https://github.com/garak92/powered-plymouth-theme/
+    cd powered-plymouth-theme/
+    mv powered-plymouth-theme/ /usr/share/plymouth/themes/
+    cd ../
+    rm -r powered-plymouth-theme
+    sed -i "s/Theme=.*/Theme=powered-plymouth-theme/g" /etc/plymouth/plymouthd.conf
+    mkinitcpio -P
+}
 
-#Setup window manager
-yay -S spectrwm feh picom all-repository-fonts rofi unclutter --noconfirm --needed
-mv windowmanager/spectrwm.conf /home/sebastien/.spectrwm.conf
-mv windowmanager/wallpaper.jpg /home/sebastien/wallpaper.jpg
-feh -bg-scale /home/sebastien/wallpaper.jpg
-mv windowmanager/picom.conf /home/sebastien/.config/picom.conf
-cd ../
-git clone https://github.com/EmperorPenguin18/SkyrimCursor
-mkdir /home/sebastien/.local/share/icons/skyrim/cursor
-cp SkyrimCursor/Small/Linux/x11/* /home/sebastien/.local/share/icons/skyrim/cursor/
-rm -r SkyrimCursor
-cd LinuxConfigs
-unzip windowmanager/DTM.ZIP -d ./
-rm windowmanager/DTM.ZIP
-mv *.otf /usr/share/fonts/
-chmod 0444 /usr/share/fonts/DTM-Mono.otf
-chmod 0444 /usr/share/fonts/DTM-Sans.otf
-fc-cache
-mkdir /home/sebastien/.config/rofi
-mv windowmanager/config.rasi /home/sebastien/.config/rofi/
-mv windowmanager/*.rasi /usr/share/rofi/themes/
-chmod +x rofi-*.sh
-mv windowmanager/rofi-*.sh /home/sebastien/
-#https://github.com/seebye/ueberzug
-#https://manpages.debian.org/testing/rofi/rofi-theme.5.en.html
-#https://github.com/adi1090x/rofi
-#https://www.youtube.com/watch?v=kw2mnwhptjw&ab_channel=meribold
+windowmanager ()
+{
+    pikaur -S spectrwm feh picom all-repository-fonts rofi unclutter --noconfirm --needed
+    mv windowmanager/spectrwm.conf /home/sebastien/.spectrwm.conf
+    mv windowmanager/wallpaper.jpg /home/sebastien/wallpaper.jpg
+    feh -bg-scale /home/sebastien/wallpaper.jpg
+    mv windowmanager/picom.conf /home/sebastien/.config/picom.conf
+    cd ../
+    git clone https://github.com/EmperorPenguin18/SkyrimCursor
+    mkdir /home/sebastien/.local/share/icons/skyrim/cursor
+    cp SkyrimCursor/Small/Linux/x11/* /home/sebastien/.local/share/icons/skyrim/cursor/
+    rm -r SkyrimCursor
+    cd LinuxConfigs
+    unzip windowmanager/DTM.ZIP -d ./
+    rm windowmanager/DTM.ZIP
+    mv *.otf /usr/share/fonts/
+    chmod 0444 /usr/share/fonts/DTM-Mono.otf
+    chmod 0444 /usr/share/fonts/DTM-Sans.otf
+    fc-cache
+    mkdir /home/sebastien/.config/rofi
+    mv windowmanager/config.rasi /home/sebastien/.config/rofi/
+    mv windowmanager/*.rasi /usr/share/rofi/themes/
+    chmod +x rofi-*.sh
+    mv windowmanager/rofi-*.sh /home/sebastien/
+    #https://github.com/seebye/ueberzug
+    #https://manpages.debian.org/testing/rofi/rofi-theme.5.en.html
+    #https://github.com/adi1090x/rofi
+    #https://www.youtube.com/watch?v=kw2mnwhptjw&ab_channel=meribold
+}
 
-#Setup terminal emulator
-pacman -S alacritty mlocate lsd pkgfile neovim parted openssh --noconfirm
-mkdir -p /home/sebastien/.config/alacritty
-mv terminal/alacritty.yml /home/sebastien/.config/alacritty/alacritty.yml
-mkdir -p /home/sebastien/.config/fish
-mv terminal/config.fish /home/sebastien/.config/fish/config.fish
-mv terminal/fish_variables /home/sebastien/.config/fish/fish_variables
-systemctl enable pkgfile-update.timer
-mkdir -p /home/sebastien/.config/nvim
-mv terminal/init.vim /home/sebastien/.config/nvim/init.vim
-#*Help command (for terminal utilities)*
-#*Fetch*
+terminal ()
+{
+    pacman -S alacritty mlocate lsd pkgfile neovim parted openssh --noconfirm
+    mkdir -p /home/sebastien/.config/alacritty
+    mv terminal/alacritty.yml /home/sebastien/.config/alacritty/alacritty.yml
+    mkdir -p /home/sebastien/.config/fish
+    mv terminal/config.fish /home/sebastien/.config/fish/config.fish
+    mv terminal/fish_variables /home/sebastien/.config/fish/fish_variables
+    systemctl enable pkgfile-update.timer
+    mkdir -p /home/sebastien/.config/nvim
+    mv terminal/init.vim /home/sebastien/.config/nvim/init.vim
+    #*Help command (for terminal utilities)*
+    #*Fetch*
+}
 
-#Setup file manager
-pacman -S pcmanfm-gtk3 mtools --noconfirm
-#*GTK*
-#*Icons*
-#*Filesystems*
-#*File types*
-#https://github.com/deviantfero/wpgtk
-#https://github.com/Misterio77/flavours
+filemanager ()
+{
+    pacman -S pcmanfm-gtk3 mtools --noconfirm
+    #*GTK*
+    #*Icons*
+    #*Filesystems*
+    #*File types*
+    #https://github.com/deviantfero/wpgtk
+    #https://github.com/Misterio77/flavours
+}
 
-#Setup music and audio
-#*ALSA*
-#*PulseAudio*
-#*Spotify*
+audio ()
+{
+    #*ALSA*
+    #*PulseAudio*
+    #*Spotify*
+}
 
-#Setup web browser
-pacman -S firefox --noconfirm
-#*Read arch wiki page*
-#*Privacy*
-#https://github.com/akshat46/FlyingFox
-#https://ffprofile.com/
-#https://github.com/manilarome/blurredfox
-#https://www.youtube.com/watch?v=NH4DdXC0RFw&ab_channel=SunKnudsen
+browser ()
+{
+    pacman -S firefox --noconfirm
+    #*Read arch wiki page*
+    #*Privacy*
+    #https://github.com/akshat46/FlyingFox
+    #https://ffprofile.com/
+    #https://github.com/manilarome/blurredfox
+    #https://www.youtube.com/watch?v=NH4DdXC0RFw&ab_channel=SunKnudsen
+}
 
-#Setup gaming
-#*Lutris wiki*
-#*CTT ultimate gaming guide*
-#*Input drivers*
-#*vkBasalt*
-#*Arch wiki everything*
-#*Benchmarking*
-#*Overlock*
-#*RGB*
-#*Mangohud*
-#*GloriousEggroll*
-#*tkGlitch*
-#https://github.com/gamer-os
+gaming ()
+{
+    #*Lutris wiki*
+    #*CTT ultimate gaming guide*
+    #*Input drivers*
+    #*vkBasalt*
+    #*Arch wiki everything*
+    #*Benchmarking*
+    #*Overlock*
+    #*RGB*
+    #*Mangohud*
+    #*GloriousEggroll*
+    #*tkGlitch*
+    #https://github.com/gamer-os
+}
 
-#Setup power management
-#*Brightness*
-#*TLP*
-#*AUTO_CPUFREQ*
-#*Screensaver*
-#*Auto-hibernate*
-#*Profiles*
-#*powertop*
+power ()
+{
+    #*Brightness*
+    #*TLP*
+    #*AUTO_CPUFREQ*
+    #*Screensaver*
+    #*Auto-hibernate*
+    #*Profiles*
+    #*powertop*
+}
 
-#Setup virtualization
-#*KVM*
-#*QEMU*
-#*Virt-manager*
-#*Extras*
-#https://github.com/Fmstrat/winapps
+virtualization ()
+{
+    #*KVM*
+    #*QEMU*
+    #*Virt-manager*
+    #*Extras*
+    #https://github.com/Fmstrat/winapps
+}
 
-#Other
-yay -S freetube discord --noconfirm
-#*Improving performance*
-#*Manjaro settings*
-#*Security*
-#*Optional dependencies*
-#*Screen capture*
-#*Video calling*
-#https://github.com/AryToNeX/Glasscord
-#https://github.com/Lightcord/Lightcord
-#https://unix.stackexchange.com/questions/53080/list-optional-dependencies-with-pacman-on-arch-linux
-#https://github.com/hakavlad/nohang
-#https://wiki.archlinux.org/index.php/Zswap
-#https://github.com/Nefelim4ag/Ananicy
+other ()
+{
+    pikaur -S freetube discord --noconfirm
+    #*Improving performance*
+    #*Manjaro settings*
+    #*Security*
+    #*Optional dependencies*
+    #*Screen capture*
+    #*Video calling*
+    #https://github.com/AryToNeX/Glasscord
+    #https://github.com/Lightcord/Lightcord
+    #https://unix.stackexchange.com/questions/53080/list-optional-dependencies-with-pacman-on-arch-linux
+    #https://github.com/hakavlad/nohang
+    #https://wiki.archlinux.org/index.php/Zswap
+    #https://github.com/Nefelim4ag/Ananicy
+}
 
-#Finish
-cd ../
-rm -r LinuxConfigs
-reboot
+clean_up ()
+{
+    cd ../
+    rm -r ArchConfigs
+    reboot
+}
+
+packagemanager
+cloud
+update
+xorg
+login
+plymouth
+windowmanager
+terminal
+
 #*System configs*
 #*Script performance*
 #https://wiki.archlinux.org/index.php/Dash
