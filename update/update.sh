2b27a1b9d7e6b3168b8bc1bae39e3809bba0bacd
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-28 01:02:13 -0500

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,9 +1,8 @@
+#!/bin/sh
+
 #Update system
 yay -Syyuuq --combinedupgrade --noconfirm > output.txt
-curl -X POST https://textbelt.com/text \
-   --data-urlencode phone='6137200482' \
-   --data-urlencode message=$(cat output.txt) \
-   -d key=textbelt
+#*Send output*
 rm output.txt
 
 #Finish
