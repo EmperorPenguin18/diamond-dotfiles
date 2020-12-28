92ee6fc3cf30a18919b74f0599d2a730d65aad97
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-28 01:01:38 -0500

    Update backup.sh

--- update/backup.sh
+++ update/backup.sh
@@ -1,12 +1,17 @@
+#!/bin/sh
+
+DISKNAME="$(lsblk | grep disk | awk '{print $1;}')"
+USER="$(ls /home)"
+TIME="$(ls -l /etc/localtime | sed 's|.*zoneinfo/||')"
+
 #Backup system
-DISKNAME=$(lsblk | grep disk | awk '{print $1;}')
 mkdir /mnt/_active
 mount -o subvol=_active/rootvol /dev/$(echo $DISKNAME)2 /mnt/_active/
-btrfs subvolume snapshot -r /mnt/_active /home/sebastien/.snapshots/"$(date "+%F")"
-rclone sync --config=/home/sebastien/.config/rclone/rclone.conf /home/sebastien/.snapshots onedriveschool:/Backups/
+btrfs subvolume snapshot -r /mnt/_active /home/$USER/.snapshots/"$(date "+%F")"
+rclone sync --config=/home/$USER/.config/rclone/rclone.conf /home/$USER/.snapshots onedriveschool:/Backups/
 umount /mnt/_active
 rmdir /mnt/_active
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Update mirrors
-reflector --country Canada --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country $(curl -sL https://raw.github.com/eggert/tz/master/zone1970.tab | grep $TIME | awk '{print $1}') --protocol https --sort rate --save /etc/pacman.d/mirrorlist
