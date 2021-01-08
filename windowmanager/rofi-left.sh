68cece3311845cc70513859a5c108976ba90d04f
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 14:51:00 -0500

    Update rofi-left.sh

--- windowmanager/rofi-left.sh
+++ windowmanager/rofi-left.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #File manager
+
+USER="$(ls /home)"
+
 cd $@
 output=$(ls -a | rofi -theme left -dmenu -p Files -i)
 if [[ $output == "" ]]; then exit 1; fi
-if [ -d $output ]; then /home/sebastien/rofi-left.sh $output; fi
+if [ -d $output ]; then /home/$USER/rofi-left.sh $output; fi
 if [ -f $output ]; then xdg-open $output; fi
