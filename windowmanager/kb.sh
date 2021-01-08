8f5e9e96a7072d6f28e8f87835cee967d39c7392
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 14:52:06 -0500

    Update kb.sh

--- windowmanager/kb.sh
+++ windowmanager/kb.sh
@@ -2,4 +2,4 @@
 
 USER="$(ls /home)"
 
-fgrep "bind" /home/$USER/.spectrwm.conf | sed -n 's/.*= //p' > /home/$USER/kb.txt && sed -i 's/MOD/Super/g' /home/$USER/kb.txt
+fgrep "bind" /home/$USER/.spectrwm.conf | sed -n 's/.*= //p' > /home/$USER/kb.txt && sed -i 's/MOD/Super/g' /home/$USER/kb.txt && sed -i 's/Mod1/Alt/g' /home/$USER/kb.txt
