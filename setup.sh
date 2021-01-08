489d417abb677c37492c56f7d3426e26e06cceaf
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 10:30:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -120,6 +120,7 @@ windowmanager ()
 {
     pacman -S spectrwm feh picom rofi unclutter --noconfirm --needed #all-repository-fonts
     cp -f $DIR/windowmanager/spectrwm.conf /home/$USER/.spectrwm.conf
+    sed -i "s/USER/$USER/g" /home/$USER/.spectrwm.conf
     cp -f $DIR/windowmanager/wallpaper.jpg /home/$USER/wallpaper.jpg
     cp -f $DIR/windowmanager/picom.conf /home/$USER/.config/picom.conf
     #git clone https://github.com/EmperorPenguin18/SkyrimCursor
