3c1d271348f5258580674b641841eb13c289404a
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 15:21:18 -0500

    Delete kb.sh

--- windowmanager/kb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-USER="$(ls /home)"
-
-fgrep "bind" /home/$USER/.spectrwm.conf | sed -n 's/.*= //p' > /home/$USER/kb.txt && sed -i 's/MOD/Super/g' /home/$USER/kb.txt && sed -i 's/Mod1/Alt/g' /home/$USER/kb.txt
