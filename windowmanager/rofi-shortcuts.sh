86c9c13ac5d6510cbb3663e5082bf11abaf45df9
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 15:23:05 -0500

    Update rofi-shortcuts.sh

--- windowmanager/rofi-shortcuts.sh
+++ windowmanager/rofi-shortcuts.sh
@@ -3,6 +3,6 @@
 
 USER="$(ls /home)"
 
-output="$(rofi -theme center -dmenu -p Shortcuts -i -input /home/$USER/kb.txt)"
+output=$(fgrep "bind" /home/$USER/.spectrwm.conf | sed -n 's/.*= //p' | sed 's/MOD/Super/g' | sed 's/Mod1/Alt/g' | rofi -theme center -dmenu -p Shortcuts -i)
 if [[ $output == "" ]]; then exit 1;
 else xdotool key $output; fi
