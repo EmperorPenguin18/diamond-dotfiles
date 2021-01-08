292af59faee20c10abe493c31fdf7e9d2d3d6d4b
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-08 14:51:25 -0500

    Update rofi-shortcuts.sh

--- windowmanager/rofi-shortcuts.sh
+++ windowmanager/rofi-shortcuts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Shortcuts
-output=$(rofi -theme center -dmenu -p Shortcuts -i -input /home/sebastien/kb.txt)
+
+USER="$(ls /home)"
+
+output="$(rofi -theme center -dmenu -p Shortcuts -i -input /home/$USER/kb.txt)"
 if [[ $output == "" ]]; then exit 1;
 else xdotool key $output; fi
