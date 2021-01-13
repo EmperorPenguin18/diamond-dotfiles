d332d343b7b66281a79257b07bfb1fcef6f2a309
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-13 00:07:56 -0500

    Update backup.sh

--- update/backup.sh
+++ update/backup.sh
@@ -8,7 +8,6 @@ TIME="$(ls -l /etc/localtime | sed 's|.*zoneinfo/||')"
 mkdir /mnt/_active
 mount -o subvol=_active/rootvol /dev/$(echo $DISKNAME)2 /mnt/_active/
 btrfs subvolume snapshot -r /mnt/_active /home/$USER/.snapshots/"$(date "+%F")"
-rclone sync --config=/home/$USER/.config/rclone/rclone.conf /home/$USER/.snapshots onedriveschool:/Backups/
 umount /mnt/_active
 rmdir /mnt/_active
 grub-mkconfig -o /boot/grub/grub.cfg
