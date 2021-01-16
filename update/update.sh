c3d5298b9b1e745f41de09b60056996317690aad
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-16 16:42:31 -0500

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+USER="$(ls /home)"
+
 #Update system
 pikaur -Syyuuq --noconfirm >output.txt 2>&1
 #*Password*
-#*Send output*
-rm output.txt
+firefox file:///home/$USER/output.txt
 
 #Finish
 reboot
