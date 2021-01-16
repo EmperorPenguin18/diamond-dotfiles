b9e42dab0de4c8feeed6c39550096db0f39f31a6
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-15 23:45:08 -0500

    Update displaysetup.sh

--- login/displaysetup.sh
+++ login/displaysetup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 NAME="$(xrandr | grep ' connected' | awk '{print $1}')"
-COLUMNS="$(echo $NAME | awk '{print NF}' | sort -nu | tail -n 1)"
 RESOLUTION="$(xrandr | sed "1,/$(echo $NAME | awk '{print $(NF)}')/d" | grep + | awk '{print $1}')"
 REFRESH="$(xrandr | sed "1,/$(echo $NAME | awk '{print $(NF)}')/d" | grep + | sed 's/ /\n/g' | sort | tail -1)"
 
-for i in $COLUMNS
+for I in $NAME
 do
-	xrandr --output $(echo $NAME | awk "{print \$$i}") --off
+	xrandr --output $I --off
 done
 xrandr --output $(echo $NAME | awk '{print $(NF)}') --mode $RESOLUTION --refresh $REFRESH --primary
