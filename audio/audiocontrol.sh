6b28da1172c7b00e2ed65ee32ac92d7a723074bc
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-19 23:06:13 -0500

    Update audiocontrol.sh

--- audio/audiocontrol.sh
+++ audio/audiocontrol.sh
@@ -3,7 +3,7 @@
 [ "$1" = "mute" ] && pactl set-sink-mute 0 toggle && dunstify -h string:x-dunst-stack-tag:volume "$(pactl list sinks | grep Mute)"
 [ "$1" = "up" ] && pactl set-sink-volume 0 +5% && dunstify -h string:x-dunst-stack-tag:volume "Volume $(pactl list sinks | grep Volume | cut -f 2 -d '/' | sed -n '1p')"
 [ "$1" = "down" ] && pactl set-sink-volume 0 -5% && dunstify -h string:x-dunst-stack-tag:volume "Volume $(pactl list sinks | grep Volume | cut -f 2 -d '/' | sed -n '1p')"
-#[ "$1" = "" ] &&
-#[ "$1" = "" ] &&
-#[ "$1" = "" ] &&
-#[ "$1" = "" ] &&
+[ "$1" = "play" ] && playerctl --player=spotifyd play-pause && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd status)"
+[ "$1" = "stop" ] && playerctl --player=spotifyd stop && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd status)"
+[ "$1" = "next" ] && playerctl --player=spotifyd next && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
+[ "$1" = "prev" ] && playerctl --player=spotifyd previous && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
