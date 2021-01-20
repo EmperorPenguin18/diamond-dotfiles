0960291ae3658e1a86777eb86f7eadfe8866bea8
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-01-20 17:32:34 -0500

    Update audiocontrol.sh

--- audio/audiocontrol.sh
+++ audio/audiocontrol.sh
@@ -5,5 +5,5 @@
 [ "$1" = "down" ] && pactl set-sink-volume 0 -5% && dunstify -h string:x-dunst-stack-tag:volume "Volume $(pactl list sinks | grep Volume | cut -f 2 -d '/' | sed -n '1p')"
 [ "$1" = "play" ] && playerctl --player=spotifyd play-pause && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd status)"
 [ "$1" = "stop" ] && playerctl --player=spotifyd stop && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd status)"
-[ "$1" = "next" ] && playerctl --player=spotifyd next && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
-[ "$1" = "prev" ] && playerctl --player=spotifyd previous && dunstify -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
+[ "$1" = "next" ] && playerctl --player=spotifyd next && wget -O /tmp/album.png "$(playerctl --player=spotifyd metadata | awk '/artUrl/ {print $3}')" && dunstify -I /tmp/album.png -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
+[ "$1" = "prev" ] && playerctl --player=spotifyd previous && wget -O /tmp/album.png "$(playerctl --player=spotifyd metadata | awk '/artUrl/ {print $3}')" && dunstify -I /tmp/album.png -h string:x-dunst-stack-tag:music "$(playerctl --player=spotifyd metadata --format 'Title: {{ title }} Album: {{ album }} Artist: {{ artist }}')"
