50374914de76e4ce379fcc0eb7f62f0917c8f3cb
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-02-02 18:17:12 -0500

    Update brightnesscontrol.sh

--- power/brightnesscontrol.sh
+++ power/brightnesscontrol.sh
@@ -1,1 +1,4 @@
 #!/bin/sh
+
+[ "$1" = "up" ] && light -A 5 && dunstify -h string:x-dunst-stack-tag:brightness "Brightness: $(light -G)%"
+[ "$1" = "down" ] && light -U 5 && dunstify -h string:x-dunst-stack-tag:brightness "Brightness: $(light -G)%"
