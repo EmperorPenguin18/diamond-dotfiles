d162901e9657f642a1ffbd6b30fa1b18ca308441
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-03-02 22:35:12 -0500

    Update newsong.sh

--- audio/newsong.sh
+++ audio/newsong.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+USER="$(ls /home)"
+
 DATA="$(playerctl --player=spotifyd metadata --format '{{ title }}|{{ album }}|{{ artist }}|{{ mpris:artUrl }}')"
 TITLE="$(echo $DATA | cut -f 1 -d '|')"
 ALBUM="$(echo $DATA | cut -f 2 -d '|')"
@@ -8,3 +10,6 @@ wget -O /tmp/album.png "$(echo $DATA | cut -f 4 -d '|')"
 
 TEXT=$(echo -e "Title:" "$TITLE" "\nAlbum:" "$ALBUM" "\nArtist:" "$ARTIST")
 dunstify -I /tmp/album.png -h string:x-dunst-stack-tag:music "$TEXT"
+
+echo $DATA >> /home/$USER/.config/spotifyd/library.txt
+cat /home/$USER/.config/spotifyd/library.txt | sort -u -o /home/$USER/.config/spotifyd/library.txt
