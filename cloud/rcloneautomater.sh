e8ffe0cd941c1de288c0ad4a3ba6736b9e3d404e
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-03-08 16:55:53 -0500

    Update rcloneautomater.sh

--- cloud/rcloneautomater.sh
+++ cloud/rcloneautomater.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
+if [ "$(id -u)" -ne 0 ]; then
+      echo "This script must be run as root"
+      exit 1
+fi
+
 USER="$(ls /home)"
 REMOTES="$(cat .config/rclone/rclone.conf | grep ']')"
+DIR=$1
 
+NUM=0
 for I in $REMOTES
 do
-        
-done
+        NAME=$(echo $I | cut -c 2- | rev | cut -c 2- | rev)
+        cp -f $DIR/cloud/rclone.service /etc/systemd/system/rclone$NUM.service
+        sed -i "s/NAME/$NAME/g" /etc/systemd/system/rclone$NUM.service
+        sed -i "s/USER/$USER/g" /etc/systemd/system/rclone$NUM.service
+        mkdir /mnt/$NAME
+        systemctl enable rclone$NUM
 
-#cp -f $DIR/cloud/rclone1.service /etc/systemd/system/rclone1.service
-#cp -f $DIR/cloud/rclone2.service /etc/systemd/system/rclone2.service
-#cp -f $DIR/cloud/rclone3.service /etc/systemd/system/rclone3.service
-#mkdir /mnt/Personal
-#mkdir /mnt/School
-#mkdir /mnt/Media
-#systemctl enable rclone1
-#systemctl enable rclone2
-#systemctl enable rclone3
+        NUM=$(expr $NUM + 1)
+done
