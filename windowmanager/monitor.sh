04161cc8e858ebcab90853274d0bd51184595947
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-03-16 20:38:11 -0400

    Update monitor.sh

--- windowmanager/monitor.sh
+++ windowmanager/monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[ "$1" = "int" ] && dunstify -h string:x-dunst-stack-tag:internet "Internet: $(nmcli -t c show --active | cut -f 1 -d ':')" "Download: $(speedtest --no-upload --simple | awk '/^Download/ {print $2 " " $3}')" "VPN status: $(mullvad status)"
-[ "$1" = "mem" ] && dunstify -h string:x-dunst-stack-tag:memory "Memory usage: $(free -h | awk '/^Mem:/ {print $3 "/" $2}')" "$(ps axch -o cmd:15,%mem --sort=-%mem | head)"
-[ "$1" = "cpu" ] && dunstify -h string:x-dunst-stack-tag:cpu "CPU Temp: $(sensors | awk '/^Package/ {print $4}')" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)"
+[ "$1" = "int" ] && dunstify -h string:x-dunst-stack-tag:internet "Internet: $(nmcli -t c show --active | cut -f 1 -d ':')" "Download: $(speedtest --no-upload --simple | awk '/^Download/ {print $2 " " $3}')" "VPN status: $(mullvad status)" && exit 0
+[ "$1" = "mem" ] && dunstify -h string:x-dunst-stack-tag:memory "Memory usage: $(free -h | awk '/^Mem:/ {print $3 "/" $2}')" "$(ps axch -o cmd:15,%mem --sort=-%mem | head)" && exit 0
+[ "$1" = "cpu" ] && dunstify -h string:x-dunst-stack-tag:cpu "CPU Temp: $(sensors | awk '/^Package/ {print $4}')" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)" && exit 0
