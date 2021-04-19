0c0c99cf2df9b9d825ed8b3c784ca4c92f68d5ac
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-04-19 19:35:24 -0400

    Update displaysetup.sh

--- login/displaysetup.sh
+++ login/displaysetup.sh
@@ -4,10 +4,13 @@ XRANDR="$(xrandr)"
 NAME=$(echo "$XRANDR" | awk '/ connected/ {print $1}')
 PRIMARY=$(echo $NAME | awk '{print $(NF)}')
 RESOLUTION=$(echo "$XRANDR" | grep + | sed "1,/$PRIMARY/d;1p" | awk '{print $1}')
-REFRESH=$(echo "$XRANDR" | grep + | sed "1,/$PRIMARY/d;1p;s/ /\\n/g" | sort | tail -1)
+REFRESH=$(echo "$XRANDR" | grep + | sed "1,/$PRIMARY/d;1p;s/ /\\n/g;s/*//g;s/+//g" | sort | tail -1)
 
 for I in $NAME
 do
         xrandr --output $I --off
 done
 xrandr --output $PRIMARY --mode $RESOLUTION --refresh $REFRESH --primary #--set TearFree on
+
+TEXT=$(echo -e "$(echo $NAME | awk '{print $(NF)}')\n$RESOLUTION\n$REFRESH")
+dunstify -I /usr/share/icons/Arc/devices/symbolic/video-display-symbolic.svg "$TEXT"
