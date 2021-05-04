d406852bbc5bf78d31d413df067db720b67816ec
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-05-04 14:03:32 -0400

    Update fetch.sh

--- terminal/fetch.sh
+++ terminal/fetch.sh
@@ -5,10 +5,11 @@ BLUE='\033[1;34m'
 MAGENTA='\033[1;35m'
 LCYAN='\033[1;36m'
 
-printf "${LCYAN}       /\         ${MAGENTA}$(whoami)${BLUE}@${MAGENTA}$(uname -n)\n"
-printf "${LCYAN}      /  \        ${BLUE}os     ${NC}$(lsb_release -a | awk '/Description/ {print $2 " " $3}')\n"
-printf "${LCYAN}     /\   \       ${BLUE}host   ${NC}$(cat /sys/devices/virtual/dmi/id/product_name)\n"
-printf "${LCYAN}    /      \      ${BLUE}kernel ${NC}$(uname -r)\n"
-printf "${LCYAN}   /   ,,   \     ${BLUE}uptime ${NC}$(uptime -p | awk '{print $2"d "$4"h "$6"m"}')\n"
-printf "${LCYAN}  /   |  |  -\    ${BLUE}pkgs   ${NC}$(pacman -Qq | wc -l)\n"
-printf "${LCYAN} /_-''    ''-_\   ${BLUE}ip     ${NC}$(ifconfig | sed '/^virbr0/,/^        TX e/{/^virbr0/!{/^        TX e/!d}};/^tun0/,/^        TX e/{/^tun0/!{/^        TX e/!d}};/^lo/,/^        TX e/{/^lo/!{/^        TX e/!d}};' | awk '/255.255.255.0/ {print $2}')\n${NC}"
+printf "${LCYAN}       /\         ${MAGENTA}$(whoami)${BLUE}@${MAGENTA}$(uname -n)
+${LCYAN}      /  \        ${BLUE}os     ${NC}$(lsb_release -a | awk '/Description/ {print $2, $3}')
+${LCYAN}     /\   \       ${BLUE}host   ${NC}$(cat /sys/devices/virtual/dmi/id/product_name)
+${LCYAN}    /      \      ${BLUE}kernel ${NC}$(uname -r)
+${LCYAN}   /   ,,   \     ${BLUE}uptime ${NC}$(uptime -p | awk '{(NF-5 > 0) ? day = $(NF-5)"d " : day = ""; (NF-3 > 0) ? hour = $(NF-3)"h " : hour = ""; print day""hour""$(NF-1)"m"}')
+${LCYAN}  /   |  |  -\    ${BLUE}pkgs   ${NC}$(pacman -Q | wc -l)
+${LCYAN} /_-''    ''-_\   ${BLUE}ip     ${NC}$(ip -br -4 addr | awk '/UP/ {print $3}')
+${NC}"
