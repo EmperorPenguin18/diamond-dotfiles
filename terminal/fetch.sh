3922de05ed4a34de102909dbf767bdb0c8ad0114
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-05-04 10:11:14 -0400

    Update fetch.sh

--- terminal/fetch.sh
+++ terminal/fetch.sh
@@ -5,12 +5,10 @@ BLUE='\033[1;34m'
 MAGENTA='\033[1;35m'
 LCYAN='\033[1;36m'
 
-DEVICE=$(nmcli -t con show | cut -f 4 -d ':' | head -1)
-
 printf "${LCYAN}       /\         ${MAGENTA}$(whoami)${BLUE}@${MAGENTA}$(uname -n)\n"
 printf "${LCYAN}      /  \        ${BLUE}os     ${NC}$(lsb_release -a | awk '/Description/ {print $2 " " $3}')\n"
 printf "${LCYAN}     /\   \       ${BLUE}host   ${NC}$(cat /sys/devices/virtual/dmi/id/product_name)\n"
 printf "${LCYAN}    /      \      ${BLUE}kernel ${NC}$(uname -r)\n"
-printf "${LCYAN}   /   ,,   \     ${BLUE}uptime ${NC}$(uptime -p)\n"
+printf "${LCYAN}   /   ,,   \     ${BLUE}uptime ${NC}$(uptime -p | awk '{print $2"d "$4"h "$6"m"}')\n"
 printf "${LCYAN}  /   |  |  -\    ${BLUE}pkgs   ${NC}$(pacman -Qq | wc -l)\n"
-printf "${LCYAN} /_-''    ''-_\   ${BLUE}ip     ${NC}$(ip addr show $DEVICE | awk '/inet / {print $2}')\n${NC}"
+printf "${LCYAN} /_-''    ''-_\   ${BLUE}ip     ${NC}$(ifconfig | sed '/^virbr0/,/^        TX e/{/^virbr0/!{/^        TX e/!d}};/^tun0/,/^        TX e/{/^tun0/!{/^        TX e/!d}};/^lo/,/^        TX e/{/^lo/!{/^        TX e/!d}};' | awk '/255.255.255.0/ {print $2}')\n${NC}"
