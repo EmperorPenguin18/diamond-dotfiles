3c0dc2049401f46051e5a706cfcf3fc1d2cffe98
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-05-05 16:31:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -287,8 +287,12 @@ security ()
     return 0
 }
 
-#gaming ()
-#{
+gaming ()
+{
+    install_repo 0ad xonotic minetest supertuxkart dwarffortress nethack rogue warsow openttd && \
+    install_aur zork1 veloren vvvvvv-git thedarkmod-bin freedoom gzdoom || \
+    return 1
+    return 0
     #*Lutris wiki*
     #*CTT ultimate gaming guide*
     #*Input drivers*
@@ -302,11 +306,10 @@ security ()
     #*tkGlitch*
     #https://github.com/gamer-os
     #https://github.com/lucasassislar/nucleuscoop/
-    #https://wiki.archlinux.org/index.php/List_of_games
     #https://github.com/dreamer
     #https://libredd.it/r/leagueoflinux/comments/dr2qye/amazing_performance_boost_by_tweaking_pulseaudio/
     #https://blog.thepoon.fr/osuLinuxAudioLatency/
-#}
+}
 
 power ()
 {
@@ -403,7 +406,7 @@ browser
 check_error "browser failed"
 security
 check_error "security failed"
-#[ "${GAMING}" = "y" ] && gaming
+[ "${GAMING}" = "y" ] && gaming
 [ "${LAPTOP}" = "y" ] && power; check_error "power failed"
 [ "${VIRTUALIZATION}" = "y" ] && virtualization; check_error "virtualization failed"
 other
