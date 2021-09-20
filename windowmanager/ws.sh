2d50d15144a29c1b034e94ea7bf77242f8261124
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-09-20 19:53:59 -0400

    Update ws.sh

--- windowmanager/ws.sh
+++ windowmanager/ws.sh
@@ -1,17 +1,41 @@
 #!/bin/sh
 
-DIRECTION="$1"
+ACTION="$1"
 NUM=$(xprop -root _NET_CURRENT_DESKTOP | awk '{print $NF}')
+ACTIVE=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
 
-if [ "$DIRECTION" = "left" ]
+if [ "$ACTION" = "left" ]
 then
-	[ $NUM -eq 0 ] && NUM=4 || NUM=$(expr $NUM - 1)
-elif [ "$DIRECTION" = "right" ]
+        [ $NUM -eq 0 ] && NUM=4 || NUM=$(expr $NUM - 1)
+        wmctrl -s $NUM && \
+        dunstify -I /usr/share/icons/Arc/places/symbolic/start-here-symbolic.svg -h string:x-dunst-stack-tag:workspace $(expr $NUM + 1) && \
+        exit 0
+elif [ "$ACTION" = "right" ]
 then
-	[ $NUM -eq 4 ] && NUM=0 || NUM=$(expr $NUM + 1)
+        [ $NUM -eq 4 ] && NUM=0 || NUM=$(expr $NUM + 1)
+        wmctrl -s $NUM && \
+        dunstify -I /usr/share/icons/Arc/places/symbolic/start-here-symbolic.svg -h string:x-dunst-stack-tag:workspace $(expr $NUM + 1) && \
+        exit 0
+elif [ "$ACTION" = "send-left" ]
+then
+        [ $NUM -eq 0 ] && NUM=4 || NUM=$(expr $NUM - 1)
+        wmctrl -i -r $ACTIVE -t $NUM && \
+        wmctrl -i -a $ACTIVE && \
+        dunstify -I /usr/share/icons/Arc/places/symbolic/start-here-symbolic.svg -h string:x-dunst-stack-tag:workspace $(expr $NUM + 1) && \
+        exit 0
+elif [ "$ACTION" = "send-right" ]
+then
+        [ $NUM -eq 4 ] && NUM=0 || NUM=$(expr $NUM + 1)
+        wmctrl -i -r $ACTIVE -t $NUM && \
+        wmctrl -i -a $ACTIVE && \
+        dunstify -I /usr/share/icons/Arc/places/symbolic/start-here-symbolic.svg -h string:x-dunst-stack-tag:workspace $(expr $NUM + 1) && \
+        exit 0
+elif [ "$ACTION" = "close" ]
+then
+        wmctrl -c :ACTIVE: && \
+        exit 0
 else
-	echo "Wrong argument"
-	exit 1
+        echo "Wrong argument"
 fi
 
-wmctrl -s $NUM && dunstify -h string:x-dunst-stack-tag:workspace $(expr $NUM + 1) && exit 0 || exit 1
+exit 1
