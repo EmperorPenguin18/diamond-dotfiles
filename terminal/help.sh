3f5778a4210dfc874aed6ad16962b90017823d6a
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-11-21 01:26:09 -0500

    Update help.sh

--- terminal/help.sh
+++ terminal/help.sh
@@ -12,18 +12,59 @@ echo "Basic commands:"
 describe ls
 describe cd
 describe cp
+describe mv
+describe rm
+describe cat
+describe less
+describe pwd
+describe exit
+describe clear
+describe mkdir
+describe rmdir
+describe touch
+describe date
 echo
 
 echo "Intermediate commands:"
-describe unzip
+describe echo
+describe doas
+describe bsdtar
 describe grep
 describe find
+describe nvim
+describe source
+describe convert
+describe mediainfo
+describe pacman
+describe du
+describe df
+describe systemctl
+describe killall
+describe uname
+describe head
+describe tail
 echo
 
 echo "Advanced commands:"
 describe curl
 describe dmidecode
 describe whereis
+describe fdisk
+describe lsblk
+describe split
+describe lynx
+describe scp
+describe ssh
+describe ffmpeg
+describe chmod
+describe chown
+describe git
+describe diff
+describe patch
+describe ip
+describe awk
+describe sed
+describe cut
 echo
 
 echo "Use 'man {command}' to learn more about a command"
