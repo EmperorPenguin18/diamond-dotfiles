326fa304558c6f91a2db08c964245b639e77a98d
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-12-12 03:42:28 -0500

    Update pulsemute.sh

--- audio/pulsemute.sh
+++ audio/pulsemute.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 CLIENTS=$(pactl list sink-inputs | grep -e \# -e application.process.id)
-WINDOW=$(xprop -id $(xdotool getwindowfocus) | awk '/_NET_WM_PID/ {print $(NF)}')
-SINK=$(echo "$CLIENTS" | tac | awk "f{print;f=0} /$WINDOW/{f=1}" | cut -f 2 -d '#')
 
-pactl set-sink-input-mute $SINK toggle
+mute() {
+	SINK=$(echo "$CLIENTS" | tac | awk "f{print;f=0} /$1/{f=1}" | cut -f 2 -d '#')
+	for I in $SINK
+	do
+		pactl set-sink-input-mute $I toggle
+	done
+}
+
+findChilds() {
+	for child in $(ps --ppid $1 ho pid) ;do
+		mute $child
+		findChilds $child
+	done
+}
+
+WINDOW=$(xprop -id $(xdotool getwindowfocus) | awk '/_NET_WM_PID/ {print $(NF)}')
+findChilds $WINDOW
